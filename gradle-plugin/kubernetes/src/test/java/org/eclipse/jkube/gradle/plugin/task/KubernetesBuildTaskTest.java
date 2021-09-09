68c5709768c58e42ada4e8f4d9bf1786eba32a24
Marc Nuri <marc@marcnuri.com>
2021-09-09 16:35:15 +0200

    fix: KubernetesBuildTask doesn't swallow exceptions

--- gradle-plugin/kubernetes/src/test/java/org/eclipse/jkube/gradle/plugin/task/KubernetesBuildTaskTest.java
+++ gradle-plugin/kubernetes/src/test/java/org/eclipse/jkube/gradle/plugin/task/KubernetesBuildTaskTest.java
@@ -19,13 +19,13 @@ import java.util.stream.Stream;
 
 import org.eclipse.jkube.gradle.plugin.KubernetesExtension;
 import org.eclipse.jkube.gradle.plugin.TestKubernetesExtension;
-import org.eclipse.jkube.kit.build.service.docker.DockerAccessFactory;
-import org.eclipse.jkube.kit.build.service.docker.access.DockerAccess;
 import org.eclipse.jkube.kit.config.image.ImageConfiguration;
 import org.eclipse.jkube.kit.config.image.build.BuildConfiguration;
+import org.eclipse.jkube.kit.config.service.JKubeServiceException;
+import org.eclipse.jkube.kit.config.service.kubernetes.DockerBuildService;
 
-import org.eclipse.jkube.kit.config.image.build.JKubeBuildStrategy;
 import org.gradle.api.DefaultTask;
+import org.gradle.api.GradleException;
 import org.gradle.api.Project;
 import org.gradle.api.logging.Logger;
 import org.gradle.api.plugins.JavaPluginConvention;
@@ -37,10 +37,14 @@ import org.junit.rules.TemporaryFolder;
 import org.mockito.MockedConstruction;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.junit.Assert.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockConstruction;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class KubernetesBuildTaskTest {
@@ -49,9 +53,11 @@ public class KubernetesBuildTaskTest {
   public TemporaryFolder temporaryFolder = new TemporaryFolder();
 
   private MockedConstruction<DefaultTask> defaultTaskMockedConstruction;
-  private MockedConstruction<DockerAccessFactory> dockerAccessFactoryMockedConstruction;
+  private MockedConstruction<DockerBuildService> dockerBuildServiceMockedConstruction;
   private Project project;
   private KubernetesExtension extension;
+  private boolean isBuildServiceApplicable;
+  private boolean isBuildError;
 
   @Before
   public void setUp() throws IOException {
@@ -60,20 +66,21 @@ public class KubernetesBuildTaskTest {
       when(mock.getProject()).thenReturn(project);
       when(mock.getLogger()).thenReturn(mock(Logger.class));
     });
-    dockerAccessFactoryMockedConstruction = mockConstruction(DockerAccessFactory.class, (mock, ctx) -> {
-      when(mock.createDockerAccess(any())).thenReturn(mock(DockerAccess.class));
+    dockerBuildServiceMockedConstruction = mockConstruction(DockerBuildService.class, (mock, ctx) -> {
+      when(mock.isApplicable()).thenReturn(isBuildServiceApplicable);
+      if (isBuildError) {
+        doThrow(new JKubeServiceException("Exception during Build")).when(mock).build(any());
+      }
     });
+    isBuildServiceApplicable = true;
+    isBuildError = false;
     extension = new TestKubernetesExtension();
     when(project.getProjectDir()).thenReturn(temporaryFolder.getRoot());
     when(project.getBuildDir()).thenReturn(temporaryFolder.newFolder("build"));
     when(project.getConfigurations().stream()).thenAnswer(i -> Stream.empty());
     when(project.getBuildscript().getConfigurations().stream()).thenAnswer(i -> Stream.empty());
-    when(project.getProperties()).thenReturn(Collections.emptyMap());
     when(project.getExtensions().getByType(KubernetesExtension.class)).thenReturn(extension);
     when(project.getConvention().getPlugin(JavaPluginConvention.class)).thenReturn(mock(JavaPluginConvention.class));
-    when(project.getGroup()).thenReturn("org.eclipse.jkube");
-    when(project.getName()).thenReturn("test-build-task");
-    when(project.getVersion()).thenReturn("0.0.1-SNAPSHOT");
     extension.images = Collections.singletonList(ImageConfiguration.builder()
         .name("foo/bar:latest")
         .build(BuildConfiguration.builder()
@@ -84,32 +91,54 @@ public class KubernetesBuildTaskTest {
 
   @After
   public void tearDown() {
-    dockerAccessFactoryMockedConstruction.close();
+    dockerBuildServiceMockedConstruction.close();
     defaultTaskMockedConstruction.close();
   }
 
   @Test
-  public void runTask_withImageConfiguration_shouldGetBuildService() {
+  public void runTask_withImageConfiguration_shouldRunBuild() throws JKubeServiceException {
     // Given
     final KubernetesBuildTask buildTask = new KubernetesBuildTask(KubernetesExtension.class);
     // When
     buildTask.runTask();
     // Then
-    assertThat(buildTask.resolvedImages)
-        .singleElement()
-        .hasFieldOrPropertyWithValue("name", "foo/bar:latest");
+    assertThat(buildTask.jKubeServiceHub.getBuildService()).isNotNull()
+        .isInstanceOf(DockerBuildService.class);
+    verify(buildTask.jKubeServiceHub.getBuildService(), times(1)).build(any());
   }
 
   @Test
-  public void runTask_withImageConfigurationAndJibBuildStrategy_shouldGetBuildService() {
+  public void runTask_withEmptyImageConfigurations_shouldNotRunBuild() throws JKubeServiceException {
     // Given
-    extension.buildStrategy = JKubeBuildStrategy.jib;
+    extension.images = Collections.emptyList();
     final KubernetesBuildTask buildTask = new KubernetesBuildTask(KubernetesExtension.class);
     // When
     buildTask.runTask();
     // Then
-    assertThat(buildTask.resolvedImages)
-      .singleElement()
-      .hasFieldOrPropertyWithValue("name", "foo/bar:latest");
+    verify(buildTask.jKubeServiceHub.getBuildService(), times(0)).build(any());
+  }
+
+  @Test
+  public void runTask_withImageConfigurationAndNoApplicableService_shouldThrowException() {
+    // Given
+    isBuildServiceApplicable = false;
+    final KubernetesBuildTask buildTask = new KubernetesBuildTask(KubernetesExtension.class);
+    // When
+    final IllegalStateException result = assertThrows(IllegalStateException.class, buildTask::runTask);
+    // Then
+    assertThat(result)
+        .hasMessage("No suitable Build Service was found for your current configuration");
+  }
+
+  @Test
+  public void runTask_withImageConfigurationAndBuildError_shouldThrowException() {
+    // Given
+    isBuildError = true;
+    final KubernetesBuildTask buildTask = new KubernetesBuildTask(KubernetesExtension.class);
+    // When
+    final GradleException result = assertThrows(GradleException.class, buildTask::runTask);
+    // Then
+    assertThat(result)
+        .hasMessage("Exception during Build");
   }
 }
